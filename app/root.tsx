b3bd1e74a34508cef6b0e2d3a6d8f9888e607843
Mikku44 <coverblack12345@gmail.com>
2026-02-18 12:31:48 +0700

    feat : add GTM

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,8 @@ export const links: Route.LinksFunction = () => [
   // },
 ];
 
+const GTM = "GTM-MCPT32KH";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -39,11 +41,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer',"${GTM}");
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
         <ScrollRestoration />
         <Scripts />
+
+         {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
